bbec520100e39edc60a21415ccd36d7414b98754
PedroCharrone <pedrocharrone@gmail.com>
2024-09-07 15:00:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [{
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal é uma skatista brasileira, conhecida como Fadinha, que conquistou o mundo com seus talentos e se tornou um ícone do esporte.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags:"skate fada sk8"
+},
+{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destacou nas competições internacionais, conquistando diversas medalhas e representando o Brasil com maestria.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" ,
+    tags:"judo judô judoca judoka"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances impecáveis, tornando-se um dos maiores nomes da ginástica artística do país.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" ,
+    tags:"ginástica ginastica ouro"
+  }
+];
+
